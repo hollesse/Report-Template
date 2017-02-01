69a4bec99b82d8315dce35ced2e3081f286e1a91
Joshua Töpfer <privat@joshuatoepfer.de>
2017-02-01 10:50:10 +0100

    fix fix and much more fix

--- ci/installtexlive.sh
+++ ci/installtexlive.sh
@@ -14,7 +14,7 @@ if ! command -v texlua > /dev/null; then
   cd install-tl-20*
 
   # Install a minimal system
-  ./install-tl --profile=./ci/texlive.profile
+  ./install-tl --profile=../support/texlive.profile
 
   cd ..
 fi
